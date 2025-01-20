5148f56db720c0784da1234e430adb7c64f64665
Dmitry <koliasam1@gmail.com>
2025-01-20 21:15:26 +0300

    add objects to scene

--- src/main/java/nzch/Jspectergame.java
+++ src/main/java/nzch/Jspectergame.java
@@ -1,8 +1,11 @@
 package nzch;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.light.Light;
+import com.jme3.light.PointLight;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Box;
@@ -21,13 +24,31 @@ public class Jspectergame extends SimpleApplication {
     @Override
     public void simpleInitApp() {
         Box b = new Box(1, 1, 1);
+        Vector3f center = new Vector3f(2,2,2);
+        Vector3f up = new Vector3f(1,1,1);
+        Box b2 = new Box(up,center);
+
+        Vector3f position = new Vector3f(3,3,3);
+        Light light = new PointLight(position);
+        light.setColor(ColorRGBA.Cyan);
+        light.setEnabled(true);
+
         Geometry geom = new Geometry("Box", b);
+        Geometry geom2 = new Geometry("Box2", b2);
 
         Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
         mat.setColor("Color", ColorRGBA.Blue);
         geom.setMaterial(mat);
 
         rootNode.attachChild(geom);
+
+        rootNode.addLight(light);
+
+        Material mat2 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        mat2.setColor("Color", ColorRGBA.Red);
+        geom2.setMaterial(mat2);
+
+        rootNode.attachChild(geom2);
     }
 
     @Override
